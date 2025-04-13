553ff7bdba56290bb21d26d94e9516ba30054bff
pierobernazar <bernazar.piero@gmail.com>
2025-04-13 15:18:46 -0400

    Add files via upload

---
+++ simulador-proyectos/src/questionsBank (1).js
@@ -0,0 +1,26 @@
+
+const questionsBank = [
+  {
+    question: "¿Qué es un proyecto?",
+    options: [
+      "Una propuesta para usar recursos sin restricciones",
+      "Un esfuerzo sin fin que busca soluciones",
+      "Una propuesta que busca satisfacer una necesidad utilizando recursos limitados",
+      "Un trámite administrativo sin objetivo claro"
+    ],
+    answer: 2
+  },
+  {
+    question: "¿Qué caracteriza a un servicio?",
+    options: [
+      "Es tangible y almacenable",
+      "Es intangible y no almacenable",
+      "Se puede ver y tocar",
+      "Tiene forma física"
+    ],
+    answer: 1
+  }
+  // Aquí irán las demás preguntas hasta 112
+];
+
+export default questionsBank;
